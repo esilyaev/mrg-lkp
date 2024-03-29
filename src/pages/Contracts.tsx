c3a169b8186cb20d8ed00f0158af463e37a9f9cf
eos <esilyaev@gmail.com>
2024-03-29 12:02:57 +0300

    examoples

--- src/pages/Contracts.tsx
+++ src/pages/Contracts.tsx
@@ -1,281 +1,119 @@
 // STYLES
-import "../assets/reset.css"
-import "../assets/common.css"
-import "../assets/contracts.css"
+import "../assets/reset.css";
+import "../assets/common.css";
+import "../assets/contracts.css";
 // COMPONENTS
 import { MRSidebar } from "../comps/MRSidebar";
-import { MRCard } from "../comps/MRCard";
 import { MRFilterButton } from "../comps/MRFilterButton";
 // MUI
-import { Grid } from "@mui/material";
-import GridOnIcon from '@mui/icons-material/GridOn';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { GridRowsProp, GridColDef, DataGrid } from "@mui/x-data-grid";
+import GridOnIcon from "@mui/icons-material/GridOn";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { rows } from "./mockContracts";
+import { useMemo, useState } from "react";
+import { ContractsList } from "./ContractsList";
+import { useNavigate } from "react-router";
 
+export enum ViewType {
+  CARD,
+  TABLE,
+}
 
-const rows: GridRowsProp = [
-  {
-    id: 1,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 2,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 3,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 4,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 5,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 6,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 7,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 8,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 9,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-  {
-    id: 10,
-    favorite: "",
-    option: "Договор Генподряда №234 ",
-    date: "12 июня 2023",
-    building: "City Bay оч. 2",
-    offer: "Генподряда",
-    name: "Оказание",
-    number: "09-87/52",
-    summ: "54 456 123.84 руб.",
-    remain: "1 654 456 123.84 руб.",
-    status: "Контроль",
-  },
-
-];
-
-const columns: GridColDef[] = [
-  {
-    field: "favorite", headerName: "", minWidth: 50,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "option", headerName: "Вид контракта", minWidth: 278,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "date", headerName: "Дата", minWidth: 140,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "building", headerName: "Проект", minWidth: 160,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "offer", headerName: "Предмет договора", minWidth: 160,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "name", headerName: "Наименование", minWidth: 160,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "number", headerName: "№", minWidth: 120,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "summ", headerName: "Сумма", minWidth: 160,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "remain", headerName: "Остаток", minWidth: 160,
-    headerClassName: 'table-header',
-  },
-  {
-    field: "status", headerName: "Статус", minWidth: 140,
-    headerClassName: 'table-header',
-    cellClassName: 'status-cell',
-  },
-
-];
-
+enum ContractsFilter {
+  ALL,
+  REVIEW,
+  CONTROL,
+  CLOSE,
+}
 
 export const Contracts = () => {
-  return <>
+  const [filter, setFilter] = useState(ContractsFilter.CLOSE);
+  const [view, setView] = useState(ViewType.TABLE);
+  const navigate = useNavigate();
+  const filteredContracts = useMemo(() => {
+    switch (filter) {
+      case ContractsFilter.ALL:
+        return rows;
 
-    <MRSidebar />
-    <section className="contracts common-p">
-      <div className="container contracts__container common-container">
-        <div className="contracts__header common-header">
-          <h1 className="contracts__title heading-1">Контракты</h1>
-          <div className="contracts__dropdown dropdown">
-            <span>Все проекты</span>
-            <ArrowDropDownIcon />
+      case ContractsFilter.CLOSE:
+        return rows.filter((item) => item.status === "Закрыто");
+
+      default:
+        return rows;
+    }
+  }, [filter]);
+  return (
+    <>
+      <MRSidebar />
+      <section className="contracts common-p">
+        <div className="container contracts__container common-container">
+          <div className="contracts__header common-header">
+            <h1 className="contracts__title heading-1">Контракты</h1>
+            <div className="contracts__dropdown dropdown">
+              <span>Все проекты</span>
+              <ArrowDropDownIcon />
+            </div>
           </div>
-        </div>
-        <div className="contracts__filter filter">
-          <ul className="filter__list">
-            <li className="filter__list-item active">
-
-              <MRFilterButton />
-
-            </li>
-            <li className="filter__list-item"><span>Согласование</span></li>
-            <li className="filter__list-item"><span>Контроль</span></li>
-            <li className="filter__list-item"><span>Закрыт</span></li>
-          </ul>
-          <div className="filter__buttons">
-            <button className="filter__buttons-item active" >
-              <div className="filter__buttons-item-wrap">
-                <GridOnIcon />
-              </div>
-            </button>
-            <button className="filter__buttons-item">
-              <div className="filter__buttons-item-wrap">
-                <FormatListBulletedIcon />
-              </div>
-            </button>
+          <div className="contracts__filter filter">
+            <ul className="filter__list">
+              <li
+                className="filter__list-item active"
+                onClick={() => {
+                  setFilter(ContractsFilter.ALL);
+                }}
+              >
+                <MRFilterButton />
+              </li>
+              <li
+                className="filter__list-item"
+                onClick={() => {
+                  setFilter(ContractsFilter.REVIEW);
+                }}
+              >
+                <span>Согласование</span>
+              </li>
+              <li
+                className="filter__list-item"
+                onClick={() => {
+                  setFilter(ContractsFilter.CONTROL);
+                }}
+              >
+                <span>Контроль</span>
+              </li>
+              <li
+                className="filter__list-item"
+                onClick={() => {
+                  setFilter(ContractsFilter.CLOSE);
+                }}
+              >
+                <span>Закрыт</span>
+              </li>
+            </ul>
+            <div className="filter__buttons">
+              <button
+                className="filter__buttons-item active"
+                onClick={() => setView(ViewType.TABLE)}
+              >
+                <div className="filter__buttons-item-wrap">
+                  <GridOnIcon />
+                </div>
+              </button>
+              <button
+                className="filter__buttons-item"
+                onClick={() => setView(ViewType.CARD)}
+              >
+                <div className="filter__buttons-item-wrap">
+                  <FormatListBulletedIcon />
+                </div>
+              </button>
+            </div>
           </div>
+          <ContractsList viewType={view} rows={filteredContracts} />
         </div>
-        <Grid container spacing={2} className="contracts__grid-list grid-list">
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-          <Grid item xs={4} className="grid-list__card card">
-            <MRCard />
-          </Grid>
-        </Grid>
+        <button onClick={() => navigate("/")}>На главную</button>
+      </section>
 
-
-        <DataGrid sx={{
-          // width: '100%',
-          background: ''
-        }} rows={rows} columns={columns} />
-
-
-      </div>
-    </section>
-
-
-    {/* <MRButton />/ */}
-
-  </>;
+      {/* <MRButton />/ */}
+    </>
+  );
 };
---
+++ src/pages/ContractsList.tsx
@@ -0,0 +1,118 @@
+import { Grid } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { MRCard } from "../comps/MRCard";
+import { ViewType } from "./Contracts";
+
+const columns: GridColDef[] = [
+  {
+    field: "favorite",
+    headerName: "",
+    minWidth: 50,
+    headerClassName: "table-header",
+  },
+  {
+    field: "option",
+    headerName: "Вид контракта",
+    minWidth: 278,
+    headerClassName: "table-header",
+  },
+  {
+    field: "date",
+    headerName: "Дата",
+    minWidth: 140,
+    headerClassName: "table-header",
+  },
+  {
+    field: "building",
+    headerName: "Проект",
+    minWidth: 160,
+    headerClassName: "table-header",
+  },
+  {
+    field: "offer",
+    headerName: "Предмет договора",
+    minWidth: 160,
+    headerClassName: "table-header",
+  },
+  {
+    field: "name",
+    headerName: "Наименование",
+    minWidth: 160,
+    headerClassName: "table-header",
+  },
+  {
+    field: "number",
+    headerName: "№",
+    minWidth: 120,
+    headerClassName: "table-header",
+  },
+  {
+    field: "summ",
+    headerName: "Сумма",
+    minWidth: 160,
+    headerClassName: "table-header",
+  },
+  {
+    field: "remain",
+    headerName: "Остаток",
+    minWidth: 160,
+    headerClassName: "table-header",
+  },
+  {
+    field: "status",
+    headerName: "Статус",
+    minWidth: 140,
+    headerClassName: "table-header",
+    cellClassName: "status-cell",
+  },
+];
+
+export const ContractsList = (props: { viewType: ViewType; rows: any[] }) => {
+  if (props.viewType === ViewType.CARD) {
+    return (
+      <Grid container spacing={2} className="contracts__grid-list grid-list">
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+        <Grid item xs={4} className="grid-list__card card">
+          <MRCard />
+        </Grid>
+      </Grid>
+    );
+  } else {
+    return (
+      <DataGrid
+        sx={{
+          // width: '100%',
+          background: "",
+        }}
+        rows={props.rows}
+        columns={columns}
+      />
+    );
+  }
+};
