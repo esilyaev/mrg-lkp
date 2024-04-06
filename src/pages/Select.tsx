8450c701aa49e51388d36489d5187e1e8784af8e
Dubrovin Maksim <gvozdb27@gmail.com>
2024-04-06 17:54:14 +0300

    contracts update

--- src/pages/Select.tsx
+++
@@ -1,9 +0,0 @@
-export const Select = (props: { items: string[] }) => {
-  return (
-    <select name="" id="">
-      {props.items.map(item => (
-        <option value={item}>{item}</option>
-      ))}
-    </select>
-  )
-}
